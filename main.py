299cf3951dfa3c6670016b33b853e8661ea48dde
questor/Inter <questor@inter>
2015-07-01 19:13:38 +0200

    small stuff

--- main.py
+++ main.py
@@ -11,33 +11,33 @@ def hashfile(afile, hasher, blocksize=65536):
     afile.close()
     return hasher.hexdigest()
 
-
-stream = open("test.yaml", "r")
-doc = yaml.load(stream)
-for k,v in doc.items():
-    print(k, "->", v)
-print("\n")
-
-
+def readconfig():
+    stream = open("test.yaml", "r")
+    doc = yaml.load(stream)
+    for k,v in doc.items():
+        print(k, "->", v)
+    print("\n")
 
 parser = argparse.ArgumentParser(description='SyncHero')
-parser.add_argument('-h', '--help', help='print usage', dest='help')
-parser.add_argument('-c', '--check-repos', help='check for updates in the repositories', dest='check')
-parser.add_argument('-s', '--sync-updates', help='check for updates and sync to new versions', dest='sync')
-#parser.add_argument('-p', '--push', help='push all repos back onto server', dest='push')
-parser.add_argument('-r', '--reverse-copy', help='reverse copy of files moved during sync', dest='revcopy')
+parser.add_argument('-c', '--check-repos', help='check for updates in the repositories', action='store_true')
+parser.add_argument('-s', '--sync-updates', help='check for updates and sync to new versions', action='store_true')
+#parser.add_argument('-p', '--push', help='push all repos back onto server', action='store_true')
+parser.add_argument('-r', '--reverse-copy', help='reverse copy of files moved during sync', action='store_true')
 args = parser.parse_args()
 
-print(hashfile(open('test.yaml', 'rb'), hashlib.md5()))
+#print(hashfile(open('test.yaml', 'rb'), hashlib.md5()))
 
-
-if args.check:
+if args.check_repos:
+    readconfig()
     pass
-elif args.sync:
+elif args.sync_updates:
+    readconfig()
     pass
-elif args.push:
-    pass
-elif args.revcopy:
+#elif args.push:
+#    readconfig()
+#    pass
+elif args.reverse_copy:
+    readconfig()
     pass
 else:
     parser.print_help()
